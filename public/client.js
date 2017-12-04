6d73afbe3272677f8f5e12855cdbb3dfd278ea79
Glitch (cord-rate) <none>
2017-12-04 07:36:28 +0000

    🍲✈️ Checkpoint public/client.js:447959 views/index.html:447959

--- public/client.js
+++ public/client.js
@@ -2,11 +2,16 @@
 
 $( window ).load(function() {
    //let pollshareRoute = `${window.location.pathname}${window.location.search}`;
-   let obj = $("#existingSpace").data("key");
+   //let obj = $("#existingSpace").data("key");
   $.get(`/get/all`, function(obj) {
-    console.log(obj);
+    let newHTML = `<ul id="polls">List of Coins:<br>`;
+    $.each(obj, function(key, value) {
+      itemID -= 1;
+      let item = data[itemID];
+      newHTML += `<li id="${itemID}poll"><button class="editButt" data-key="${item.name}" id="${itemID}editButt">${item.name}</button>`
+      newHTML += `<button class="delButt" data-key="${item.name}" id="${itemID}delButt" ${hide}>Delete</button></li>`
+      });
   });
 });
-
 
 
