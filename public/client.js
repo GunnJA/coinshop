39f769453d75c73556b7e686cad761e17b1a88c5
Glitch (cord-rate) <none>
2017-12-04 07:58:28 +0000

    🎟🤗 Checkpoint public/client.js:447959 public/style.css:447959

--- public/client.js
+++ public/client.js
@@ -4,13 +4,13 @@ $( window ).load(function() {
    //let pollshareRoute = `${window.location.pathname}${window.location.search}`;
    //let obj = $("#existingSpace").data("key");
   $.get(`/get/all`, function(obj) {
-    let newHTML = `<table class="tg"><tr><th class="tg-baqh" colspan="6">Coin Markets</th></tr><tr>`;
+    let newHTML = `<table class="tg"><tr><th class="tg-baqh" colspan="6">Coin Markets</th></tr>`;
     $.each(obj, function(key, value) {
       let item = obj[key];
-      newHTML += `<td class="tg-6k2t">${item.MarketName}</td>`;
+      newHTML += `<tr><td class="tg-6k2t">${item.MarketName}</td><td class="tg-6k2t">${item.Last}</td></tr>`;
       //newHTML += `<li id="${itemID}poll"><button class="editButt" data-key="${item.name}" id="${itemID}editButt">${item.name}</button>`
       });
-  
+  newHTML += `</tr></table>`
   $("body").append(newHTML);
   });
 });
