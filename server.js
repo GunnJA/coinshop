a1d9cdb7e2ae6cf6cb08ecacc816d27bbdad08b1
Glitch (cord-rate) <none>
2017-12-04 02:30:30 +0000

    📡💒 Checkpoint server.js:447959

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
 const express = require('express');
-const http = require("https");
 const app = express();
 const mongo = require('mongodb').MongoClient
-const dbCollectionUser = "voteruser";
-const dbCollectionPolls = "voterpolls";
+const http = require("https");
 const secret = process.env.CS_SECRET;
 const key = process.env.CS_KEY;
 let loggedIn = false;
 let offsetDefault = 10;
 let database;
 let collectCoins1;
-let collectPoll;
+let collectCoins2;
 let dump;
 
 //DB functions
 mongo.connect('mongodb://gunnja:gunnja@ds131854.mlab.com:31854/fccdb',(err, db) => {
-  if (err) throw err
+  return new Promiseif (err) throw err
   else console.log("db connection successful")
-  collectUser = db.collection(dbCollectionUser);
-  collectPoll = db.collection(dbCollectionPolls);
+  collectCoins1 = db.collection("coins1");
+  collectCoins2 = db.collection("coins2");
   database = db;
 // db.close();
 });
@@ -123,12 +121,10 @@ queryMarket().then(function(array) {
   for (let i=1; i < resultspage1.length; i+= 1) {
     let market = resultspage1[i].MarketName;
     if (market.substring(0, 3) === "BTC") {
-      console.log(market);
+      dbInsert(collectCoins1,resultspage1[i])
     }
   }
 })
-
-
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -136,96 +132,6 @@ app.use(express.static('public'));
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendfile(__dirname + '/index.html');
-});
-
-//app.get("/pollshare", function (req, res) {
-//  res.sendfile(__dirname + '/index.html');
-//  next();
-//});
-
-// Poll Functionality -------
-// new poll routing
-app.get("/new", function (req, res) {
-  let pollName = req.query.name;
-  let user = req.query.user;
-  exists(collectPoll,{"user":user,"name":pollName}).then(function(bool) {
-    if (bool) {
-      // already exists
-      res.send({'existing': bool });
-    } else {
-      dbInsert(collectPoll,{"user":user,"name":pollName});
-      res.send({'existing': bool });
-    }
-  });
-});
-
-// delete poll routing
-app.get("/delpoll", function (req, res) {
-  let pollName = req.query.name;
-  let user = req.query.user;
-  dbDelete(collectPoll,{"user":user,"name":pollName}).then(function(obj) {
-    res.send(obj);
-  });
-});
-
-// get polls
-app.get("/existing", function (req, res) {
-  if (req.query.user) {
-    dbFindAll(collectPoll,{"user": req.query.user}).then(function(obj) {
-      console.log(obj);
-      res.send(obj);
-  });
-  } else {
-    dbFindAll(collectPoll,{}).then(function(obj) {
-      console.log(obj);
-      res.send(obj);
-  });
-  }
-});
-
-// retrieve poll
-app.get("/poll", function (req, res) {
-  //let user = req.query.user;
-  let name = req.query.name;
-  console.log(name);
-  dbFindOne(collectPoll,{"name": name}).then(function(obj) {
-    console.log(obj);
-    res.send(obj);
-  });
-});
-
-
-// add options routing
-app.post("/modify", function (req, res) {
-  let pollName = req.query.name;
-  let option = req.query.option;
-  let user = req.query.user;
-  dbFindOne(collectPoll,{"name":pollName}).then(function(data) {
-    let pollObj = data;
-    if (pollObj.options) {
-      // options exists
-      if (option in pollObj.options) {
-        // new option already exists
-        res.send({"error": "option already exists"});
-      } else {
-        // new option doesnt exist
-        let options = pollObj.options;
-        options[option] = 0;
-        let newObj = {"user":user, "name" : pollName, "options" : options};
-        console.log("1st",newObj);
-        dbUpdate(user,collectPoll,newObj,pollName);
-        res.send(newObj);
-      }
-    } else {
-      // options doesnt exist
-      let options = {};
-      options[option] = 0;
-      let newObj = {"user":user, "name" : pollName, "options" : options};
-      console.log("2nd",newObj);
-      dbUpdate(user,collectPoll,newObj,pollName);
-      res.send(newObj);
-    }
-  })
 });
 
 // listen for requests :)
